9248f640a68d7df0ce28ec76e61df5f669fe630e
Christopher Chaples <33135543+chaples55@users.noreply.github.com>
2018-11-08 01:18:41 -0500

    significant progress for TA main page

---
+++ JS/scripts.js
@@ -0,0 +1,14 @@
+function windowcheck() {
+    if ($(window).height() > $(window).width()*0.9) {
+        $(view1).removeClass("leftwindow");
+        $(view2).removeClass("rightwindow");
+        $(view1).addClass("topwindow");
+        $(view2).addClass("botwindow");
+    }
+    else {
+        $(view1).addClass("leftwindow");
+        $(view2).addClass("rightwindow");
+        $(view1).removeClass("topwindow");
+        $(view2).removeClass("botwindow");
+    }
+}
