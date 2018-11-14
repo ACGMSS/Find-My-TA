346cd135b93231adbe2e4292b8d60dac2d8a36bd
Jess Smith <smith.jessk@gmail.com>
2018-11-13 22:50:29 -0500

    Add student login

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 angular.module('front-page', ['ngResource']);
 angular.module('student', ['ngResource']);
+angular.module('faculty', ['ngResource']);
 
 // this will break because 'find-my-ta' is also defined in client/faculty/app.js
-var app = angular.module('find-my-ta', ['front-page', 'student']);
+var app = angular.module('find-my-ta', ['front-page', 'student', 'faculty']);
---
+++ facultyHomeController.js
@@ -0,0 +1,15 @@
+angular.module('faculty').controller('FacultyHomeController', ['$scope', '$http', func]);
+
+function func($scope, $http) {
+    const facultyID = sessionStorage.getItem("facultyID");
+    $http.get(`/api/faculty/${facultyID}`)
+        .then(c => {
+            console.log(c);
+            return Promise.all(c.data.data.courses.map(x => {
+                return $http.get(`/api/course_section/${x.section}`);
+            }));
+        }).then(responses => {
+            console.log(responses);
+            $scope.managedCourses = responses.map(x => x.data.data);
+        }).catch(e => alert(JSON.stringify(e)));
+}
