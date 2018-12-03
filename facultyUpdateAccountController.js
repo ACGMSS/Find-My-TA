e21beb480c9853753f069741ace913e5a5b7208b
chaples55 <33135543+chaples55@users.noreply.github.com>
2018-12-03 02:32:56 -0500

    undo change

--- facultyUpdateAccountController.js
+++ facultyUpdateAccountController.js
@@ -4,7 +4,7 @@ function func($http, $scope, $resource) {
     const facultyID = sessionStorage.getItem("facultyID");
     const username = sessionStorage.getItem("facultyUsername");
     const password = sessionStorage.getItem("facultyPassword");
-    $scope.facultyUsername = facultyUsername;
+    $scope.facultyUsername = username;
     let encoded = btoa(`${username}:${password}`);
     $http.defaults.headers.common.Authorization = `Basic ${encoded}`;
 
